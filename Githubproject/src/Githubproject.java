ddc280b8c8e21fd0e78d10f24336abb228e23b75
USER <USER@HP>
2020-08-15 17:13:57 +0530

    sum is 50

--- Githubproject/src/Githubproject.java
+++ Githubproject/src/Githubproject.java
@@ -3,14 +3,16 @@ public class Githubproject {
 public static void main(String[] args) {
 	System.out.println("hello githubproject");
 	System.out.println("hello neha");
-
-
-	
-		System.out.println("hiii praful");
+	System.out.println("hiii praful");
 	System.out.println(" hii vrushali");
+	int a=20;
+	int b=30;
+	int sum=a+b;
+	System.out.println("sum = "+sum);
 	int c=80,d=60;
 	int sub=c-d;
-	System.out.println("sub = "+sub)
+	System.out.println("sub = "+sub);
+	
 	
 }
 }
